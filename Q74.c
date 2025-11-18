541ff14ced2d0ab6abd6eac6e98ff0b66f5e9038
parth-gh-dotcom <iamparthkalia2006@gmail.com>
2025-11-18 22:41:00 +0530

    100 Days Of Code

--- Q74.c
+++ Q74.c
@@ -13,7 +13,7 @@ int n1,n2;
     
     for(int i = 0; i <n1; i++){
         for(int j = 0; j <n2; j++){
-            printf("Enter (%d,%d) element: ", i+1,j+!);
+            printf("Enter (%d,%d) element: ", i+1,j+1);
             scanf("%d",&matrix[i][j]);
         }
     }
--- Q75.c
+++ Q75.c
@@ -3,19 +3,13 @@
 int main(){
     int n1,n2;
     
-    printf("Enter row size of first matrix: ");
+    printf("Enter row size of both the matrices: ");
     scanf("%d", &n1);
 
-    printf("Enter column size of first matrix: ");
+    printf("Enter column size of both the matrices: ");
     scanf("%d", &n2);
 
-    printf("Enter row size of second matrix: ");
-    scanf("%d", &n3);
-
-    printf("Enter column size of second matrix: ");
-    scanf("%d", &n4);
-
-    int matrix1[n1][n2], matrix2[n3][n4];
+    int matrix1[n1][n2], matrix2[n1][n2];
     
     for(int i = 0; i <n1; i++){
         for(int j = 0; j <n2; j++){
@@ -24,12 +18,27 @@ int main(){
         }
     }
 
-    for(int i = 0; i <n3; i++){
-        for(int j = 0; j <n4; j++){
+    for(int i = 0; i <n1; i++){
+        for(int j = 0; j <n2; j++){
             printf("Enter (%d,%d) element of the 2nd matrix: ", i+1,j+1);
-            scanf("%d",&matrix1[i][j]);
+            scanf("%d",&matrix2[i][j]);
         }
     }
-    
+
+    int sumMatrix[n1][n2];
+    for(int i = 0; i < n1; i++){
+        for(int j = 0; j < n2; j++){
+            sumMatrix[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+
+    }
+
+    for(int i = 0; i < n1; i++){
+        for(int j = 0; j < n2; j++){
+            printf("%d",sumMatrix[i][j]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
